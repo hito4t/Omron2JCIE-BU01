4a250a6f350b9a04f3a1b19cc6c3dbfc11468e34
hito4t <thitoshi@cac.co.jp>
2019-07-12 09:29:37 +0900

    Enabled to specify serial port name

--- python/Omron2JCIE_BU01Test.py
+++ python/Omron2JCIE_BU01Test.py
@@ -1,7 +1,12 @@
+import sys
 import time
 import Omron2JCIE_BU01
 
-sensor = Omron2JCIE_BU01.Omron2JCIE_BU01("/dev/ttyUSB0")
+portName = "/dev/ttyUSB0"
+if len(sys.argv) > 1:
+    portName = sys.argv[1]
+
+sensor = Omron2JCIE_BU01.Omron2JCIE_BU01(portName)
 
 for i in range(0, 5):
     sensor.update()
--- src/test/java/com/hito4t/iot/Omron2JCIE_BU01Test.java
+++ src/test/java/com/hito4t/iot/Omron2JCIE_BU01Test.java
@@ -3,7 +3,13 @@ package com.hito4t.iot;
 
 public class Omron2JCIE_BU01Test {
     public static void main(String[] args) throws Exception {
-        Omron2JCIE_BU01 sensor = new Omron2JCIE_BU01(args[0]);
+        String portName;
+        if (args.length > 0) {
+            portName = args[0];
+        } else {
+            portName = "/dev/ttyUSB0";
+        }
+        Omron2JCIE_BU01 sensor = new Omron2JCIE_BU01(portName);
         for (int i = 0; i < 5; i++) {
             sensor.update();
             System.out.println(String.format("Temperature : %.2f degC", sensor.getTemperature()));
